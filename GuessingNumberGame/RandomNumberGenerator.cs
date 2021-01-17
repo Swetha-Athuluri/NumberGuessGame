e7ddf2e0b7307e3a176a000ceeb84aa4fd4a5679
Swetha Athuluri <athuluri.swetha@gmail.com>
2021-01-16 21:05:02 -0500

    minor change

--- GuessingNumberGame/RandomNumberGenerator.cs
+++ GuessingNumberGame/RandomNumberGenerator.cs
@@ -25,7 +25,7 @@ namespace GuessingNumberGame
             {
                 // Digit number must be b/n 1 and 6 , meaning we need to generate the numbers from 2 -5
                 num = randomNumber.Next(6);
-                num = num == 0 || num == 1 ? 2 : num;
+                num = (num == 0 || num == 1) ? 2 : num;
                 generatedNum = generatedNum + num.ToString();
             }
             return generatedNum;
